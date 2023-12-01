c05c66739f1ac8869a19c04b686fa4df510ad3d0
chiichen <chiichen@qq.com>
2023-12-01 12:08:37 +0800

    引入了docSearch，实现了文档搜索功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -17,7 +18,39 @@ export default defineUserConfig({
 
     },
   },
+  plugins: [
+    docsearchPlugin({
+      appId: 'KPJEYDTI5K',
 
+      apiKey: '80f988070f3872d6d0e6595399a2a9d3',
+
+      indexName: 'chiichenio',
+
+      insights: true, // Optional, automatically send insights when user interacts with search results
+
+      // container: '### REPLACE ME WITH A CONTAINER (e.g. div) ###'
+
+      // debug: false // Set debug to true if you want to inspect the modal
+      locales: {
+        '/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+            },
+          },
+        },
+        '/en/': {
+          placeholder: 'Search Documentation',
+          translations: {
+            button: {
+              buttonText: 'Search Documentation',
+            },
+          },
+        },
+      },
+    }),
+  ],
   theme,
 
   // Enable it with pwa
